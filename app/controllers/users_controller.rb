2f571f884343147739e5ef2db8d557d8aa6bf69b
mattlaguardia <matthewlaguardia@gmail.com>
2016-10-05 11:25:56 -0700

    updates debugger branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
 
   def new
-    render :new
   end
 
   def create
-    user = User.new(user_params)
+    user = User(user_params)
+
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
